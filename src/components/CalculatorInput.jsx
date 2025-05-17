4273788a90f5b455d1c69390556490321515d758
Jose Galindo (Home) <kevin@softlogic.com.gt>
2025-05-17 12:04:23 -0600

    special class for special inputs

--- src/components/CalculatorInput.jsx
+++ src/components/CalculatorInput.jsx
@@ -3,13 +3,16 @@ import useCalculator from "@hooks/useCalculator";
 function CalculatorInput( {value} ) {
     const { onButtonClick } = useCalculator();
 
+    const getInputClass = (value) => {
+        if (isNaN(value) && value !== "+/-" && value !== ".") return "calculator-special"
+        return "calculator-button";
+    }
     return (
         <>
-            <button className="calculator-button" onClick={() => onButtonClick(value)}>
+            <button className={getInputClass(value)} onClick={() => onButtonClick(value)}>
                 {value}
             </button>
         </>
     )
 }
-
 export default CalculatorInput
